a861843486cc3530fabd6390f90970225bee5d39
Theodore Quinn <tcquinn@tcquinn.org>
2023-12-06 16:36:44 -0600

    Update handle to reflect multiple pose data collections

--- pose_db_io/handle/handle.py
+++ pose_db_io/handle/handle.py
@@ -9,24 +9,27 @@ import pose_db_io.config
 from pose_db_io.log import logger
 
 from .models.pose_2d import Pose2d
+from .models.pose_3d import Pose3d
 
 
-class Pose2dHandle:
+
+class PoseHandle:
     def __init__(self, db_uri: str = None):
         if db_uri is None:
             db_uri = pose_engine.config.Settings().MONGO_POSE_URI
 
         self.client: MongoClient = MongoClient(db_uri, uuidRepresentation="standard")
         self.db: MongoDatabase = self.client["poses"]
-        self.poses_collection: MongoCollection = self.db["poses_2d"]
+        self.poses_2d_collection: MongoCollection = self.db["poses_2d"]
+        self.poses_3d_collection: MongoCollection = self.db["poses_3d"]
 
-    def insert_poses(self, pose_2d_batch: List[Pose2d]):
+    def insert_poses_2d(self, pose_2d_batch: List[Pose2d]):
         bulk_requests = list(map(lambda p: InsertOne(p.model_dump()), pose_2d_batch))
         try:
             logger.debug(
                 f"Inserting {len(bulk_requests)} into Mongo poses_2d database..."
             )
-            self.poses_collection.bulk_write(bulk_requests, ordered=False)
+            self.poses_2d_collection.bulk_write(bulk_requests, ordered=False)
             logger.debug(
                 f"Successfully wrote {len(bulk_requests)} records into Mongo poses_2d database..."
             )
